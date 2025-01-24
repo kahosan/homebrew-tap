966e546780b6647f2489a84b95479876986198f7
kahosan <15116355657l@gmail.com>
2025-01-24 17:29:28 +0800

    fix: chatwsie url

--- Casks/chatwise.rb
+++ Casks/chatwise.rb
@@ -5,7 +5,7 @@ cask "chatwise" do
   sha256 arm:   "c8b93e14fc73892004ea1454ba2c5e836aec4bd12fea3058e9c7345e516b379d",
          intel: "da8c628c5ddb6e22b21f340828641bd4bc8c01a119ed289723eab44d614cbc6c"
 
-  url "https://github.com/egoist/chatwise-releases/releases/download/ChatWise_#{version}_#{arch}.dmg",
+  url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_#{arch}.dmg",
       verified: "github.com/egoist/chatwise-releases/"
   name "ChatWise"
   desc "The fastest AI Chatbot for any LLM"
