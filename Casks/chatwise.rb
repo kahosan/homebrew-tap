18b23f969d59d87ab301b2b8d694021227feb229
kahosan <15116355657l@gmail.com>
2025-01-26 16:56:43 +0800

    chore: update chatwise

--- Casks/chatwise.rb
+++ Casks/chatwise.rb
@@ -1,11 +1,8 @@
 cask "chatwise" do
-  arch arm: "aarch64", intel: "x64"
+  version "0.6.20"
+  sha256 "16c9929ec69a0b3ff346d14eeaf73688bf24f806cd8e278b2de3ed6801269f8c",
 
-  version "0.6.15"
-  sha256 arm:   "c8b93e14fc73892004ea1454ba2c5e836aec4bd12fea3058e9c7345e516b379d",
-         intel: "da8c628c5ddb6e22b21f340828641bd4bc8c01a119ed289723eab44d614cbc6c"
-
-  url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_#{arch}.dmg",
+  url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_aarch64.dmg",
       verified: "github.com/egoist/chatwise-releases/"
   name "ChatWise"
   desc "The fastest AI Chatbot for any LLM"
