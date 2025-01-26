ea97290be54c4a02b6a566135b1099341b62cdf2
kahosan <15116355657l@gmail.com>
2025-01-26 17:11:15 +0800

    fix: chatwise

--- Casks/chatwise.rb
+++ Casks/chatwise.rb
@@ -1,12 +1,14 @@
 cask "chatwise" do
   version "0.6.20"
-  sha256 "16c9929ec69a0b3ff346d14eeaf73688bf24f806cd8e278b2de3ed6801269f8c",
+  sha256 "16c9929ec69a0b3ff346d14eeaf73688bf24f806cd8e278b2de3ed6801269f8c"
 
   url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_aarch64.dmg",
       verified: "github.com/egoist/chatwise-releases/"
   name "ChatWise"
   desc "The fastest AI Chatbot for any LLM"
   homepage "https://chatwise.app/"
+
+  auto_updates true
 
   app "ChatWise.app"
 
