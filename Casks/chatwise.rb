9dd05b5d8c2d24a84e069aad440ccac93b8d2244
kahosan <15116355657l@gmail.com>
2025-02-28 08:59:25 +0800

    chore: update version

--- Casks/chatwise.rb
+++ Casks/chatwise.rb
@@ -1,6 +1,6 @@
 cask "chatwise" do
-  version "0.6.20"
-  sha256 "16c9929ec69a0b3ff346d14eeaf73688bf24f806cd8e278b2de3ed6801269f8c"
+  version "0.7.7"
+  sha256 "06062e6a02314d120e8b4dd84aca94ceeaa460f947f0c617226d730442562e90"
 
   url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_aarch64.dmg",
       verified: "github.com/egoist/chatwise-releases/"
--- Casks/trae.rb
+++ Casks/trae.rb
@@ -1,8 +1,8 @@
 cask "trae" do
-  version "1.0.5431"
-  sha256 "795fa8e59917455d11e416c533694485677043bb6e2b4e0c4df3d4e50b13973b"
+  version "1.0.7650"
+  sha256 "49c75113809c011cfd1908bbbfe86670f1e2b9943fef281d4f1ea9d518840dac"
 
-  url "https://lf-trae.toscdn.com/obj/trae-ai-cn/pkg/app/releases/stable/#{version}/darwin/Trae-darwin-arm64.dmg"
+  url "https://lf-cdn.trae.com.cn/obj/trae-com-cn/pkg/app/releases/stable/#{version}/darwin/Trae-darwin-arm64.dmg"
   name "Trae"
   desc "Trae is an adaptive AI IDE that transforms how you work, collaborating with you to run faster."
   homepage "https://www.trae.ai/"
